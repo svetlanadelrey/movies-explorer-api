d79892246e132b319ff2c3e07820a674f191180e
Svetlana <kuznetsova@66.ru>
2023-09-24 13:27:43 +0500

    hotfix: исправить удаление фильма

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Movie = require('../models/movie');
 const BadRequestError = require('../errors/bad-request-err');
 const NotFoundError = require('../errors/not-found-err');
-const ForbiddenError = require('../errors/forbidden-err');
 
 const getMovies = (req, res, next) => {
   const owner = req.user._id;
@@ -52,20 +51,18 @@ const createMovie = (req, res, next) => {
 
 const deleteMovie = (req, res, next) => {
   const owner = req.user._id;
-  Movie.findById(req.params._id)
+  Movie.findOneAndDelete({ _id: req.params._id, owner })
     .then((movie) => {
       if (!movie) {
         throw new NotFoundError('Фильм не найден');
       }
-      if (movie.owner.toString() !== owner) {
-        throw new ForbiddenError('Нет доступа');
-      }
-      Movie.deleteOne()
-        .then(() => res.send({ message: 'Фильм удален' }))
-        .catch(next);
+      res.send({ message: 'Фильм удален' });
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
+        return next(new BadRequestError('Введены некорректные данные'));
+      }
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
         return next(new BadRequestError('Введены некорректные данные'));
       }
       return next(err);
