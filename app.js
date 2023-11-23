bdf6b78922953254b40599822fed9bcf3397dfc3
svetlana <kuznetsova@66.ru>
2023-11-23 22:50:18 +0500

    hotfixes:cors

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   next();
