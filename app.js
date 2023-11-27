1e01e73bb3eb4dbe2cfcf5b75ec2731cfe0c9aa3
Svetlana <kuznetsova@66.ru>
2023-11-27 08:47:13 +0000

    hotfix: cors 2

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  next();
+});
 app.use(limiter);
 app.use(router);
 app.use(errorLogger);
